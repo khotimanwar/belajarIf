1243bf9747f45e378f3ff0a7a314e387b99d1a38
mikeybestdrummer <mikeybestdrummer@gmail.com>
2014-10-11 15:38:59 +0700

    belajarIf

---
+++ src/ContohIf.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anonymous
+ */
+public class ContohIf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int nilai=80;
+        
+        
+        if(nilai <=80 ){
+            System.out.println("Anda LULUS");
+        }
+        
+    }
+    
+}
